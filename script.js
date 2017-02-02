c18dc349ad9a7f37fd8a0af7e03d272f20964ed1
vivianariza <variza@cristoreyny.org>
2017-02-02 12:58:19 +0000

    my comment

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-$(document).ready(function(){
-    $("#go").click(function(){
-        var inputVal=$("#search").val();
-    }
+$(document).ready(function() {
+            $("#go").click(function() {
+                var inputVal = $("#search").val();
+                var location = $("#location").val();
+                var part1 = "https://api.foursquare.com/v2/venues/search?client_id=QVP411NNXQZSPK424MIF0VRMELDBVGU5YPP14GGGZ3Z1SVTM&client_secret=TZADRH0GKBGL4RLTCAVWX00GNLJ35PBMRDAIPLKRFUFWYXEP&v=20130815"
+                var part2 = "&near=" + location;
+                var part3 = "&query=" + search;
+                var apiUrl = part1 + part2 + part3;
+                alert(apiUrl)
+            });
+});
